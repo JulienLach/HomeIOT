32202a785aae1b39ec7bb6053d0edfae0a447053
JulienLach <leadedchamps@gmail.com>
2023-12-08 11:02:50 +0100

    close buton mobile menu et fin responsive

--- javascript/main.js
+++ javascript/main.js
@@ -1,9 +1,31 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', function () { //initier la fonction
 
-    const hamburgerButton = document.querySelector('.hamburger-button');  // ici définir les constante que je vais animer en cliquant dessus , ici le boutton et le menu en lui même
+    const iconMenu = document.querySelector('.icon-menu');
+    const iconClose = document.querySelector('.icon-close');
+
+    iconMenu.addEventListener('click', function () {  //première fonction quand je clique sur l'icone hamburger
+
+        iconMenu.style.display = 'none';
+        iconClose.style.display = 'block';
+    });
+
+    iconClose.addEventListener('click', function () {  //afficher l'icone croix
+
+        iconMenu.style.display = 'block';
+        iconClose.style.display = 'none';
+    });
+
+
+    const iconMobileMenu = document.querySelector('.icon-menu');
     const mobileMenu = document.querySelector('.mobile-menu');
 
-    hamburgerButton.addEventListener('click', () =>
+    iconMobileMenu.addEventListener('click', () =>
         mobileMenu.classList.toggle('active')
+
+    );
+
+    iconClose.addEventListener('click', () =>
+        mobileMenu.classList.toggle('active')
+
     );
 });
