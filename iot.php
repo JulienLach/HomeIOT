ddfa3107301f7622637b66d86f3cd348badbbe2f
Julien Lach <leadedchamps@gmail.com>
2024-02-07 11:46:09 +0100

    header.php et footer.php inlcude iot / kits-packs / mentions legales

---
+++ iot.php
@@ -0,0 +1,35 @@
+<?php include 'view/header.php'; ?>
+
+
+    <!-- IOT SECTION -->
+    <section class="about">
+      <div class="container">
+        <div class="about-content">
+          <div class="about-header">
+            <h1>Qu'est ce que l'IOT ?</h1>
+          </div>
+          <div class="about-infos">
+            <h3>Lorem ipsum dolor sit amet</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+              eum magnam iste voluptatibus totam nisi unde optio atque!
+              Explicabo sequi dicta deserunt maiores magnam cupiditate neque
+              voluptate sint! Deserunt, ea?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+              eum magnam iste voluptatibus totam Explicabo sequi dicta deserunt
+              maiores magnam cupiditate neque voluptate sint! Deserunt, ea?
+            </p>
+            <p>
+              Lorem ipsum consectetur adipisicing elit. Similique eum magnam
+              iste voluptatibus totam nisi unde optio atque! Explicabo sequi
+              dicta deserunt maiores magnam cupiditate neque voluptate sint!
+              Deserunt, ea?
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+<?php include 'view/footer.php'; ?>
