1ed7c7a77981e8c8f760d3f474b4ce40bbb8f1d3
Julien Lach <leadedchamps@gmail.com>
2024-02-07 11:33:00 +0100

    header.php et footer.php inlcude connexion / contact / create-account

---
+++ contact.php
@@ -0,0 +1,26 @@
+<?php include 'view/header.php'; ?>
+
+
+    <!-- CONTACT SECTION -->
+    <section class="contact">
+        <div class="container">
+            <div class="contact-content">
+                <div class="contact-header">
+                    <h1>Contact</h1>
+                </div>
+                <div>
+                    <form class="contact-form" action="">
+                        <div>
+                            <input class="text-form" type="name" name="" id="" placeholder="Nom / Prénom">
+                            <input class="text-form" type="email" name="" id="" placeholder="Email">
+                        </div>
+                        <textarea name="" id="" cols="30" rows="8" placeholder="Votre message"></textarea>
+                        <button class="contact-submit-btn" type="submit">Envoyer</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+<?php include 'view/footer.php'; ?>
---
+++ create-account.php
@@ -0,0 +1,28 @@
+<?php include 'view/header.php'; ?>
+
+
+    <!-- CREATE ACCOUNT SECTION -->
+    <section class="create-account">
+        <div class="container">
+            <div class="create-account-content">
+                <div class="create-account-header">
+                    <h1>Créer un compte</h1>
+                </div>
+                <div>
+                    <form class="create-account-form" action="">
+                        <div>
+                            <input class="text-form" type="name" name="" id="" placeholder="Nom">
+                            <input class="text-form" type="email" name="" id="" placeholder="Prénom">
+                        </div>
+                        <input class="text-form" type="email" placeholder="Email">
+                        <input class="text-form" type="password" placeholder="Mot de passe">
+                        <input class="text-form" type="password" placeholder="Confirmer le mot de passe">
+                        <button class="create-account-btn" type="submit">Créer mon compte</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+<?php include 'view/footer.php'; ?>
