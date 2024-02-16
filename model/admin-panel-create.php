ee09fe0f4fc6bcccccde200e38c847a10a5b6661
Julien Lach <leadedchamps@gmail.com>
2024-02-16 10:46:48 +0100

    create product avec liaison image ok et cle etrangère id_product

--- model/admin-panel-create.php
+++ model/admin-panel-create.php
@@ -39,10 +39,9 @@ class Product {
         $homeiot = new Database();
         $connexion = $homeiot->connect();
 
+        // Atraper les clés étangère avant de insert into le produit
+
         // Requête SQL pour ajouter un produit à la base de données
-
-        // atraper les clés étangère avant de insert into le produit
-
         $query = 'INSERT INTO products (name, price, short_desc, description, technical_sheet) VALUES (:name, :price, :short_desc, :description, :technical_sheet)';
         $statement = $connexion->prepare($query);
         $statement->bindParam(':name', $this->name);
@@ -52,15 +51,33 @@ class Product {
         $statement->bindParam(':technical_sheet', $this->technical_sheet);
         $statement->execute();
 
-        // ensuite aller choper l'id du produit que je viens d'ajouter pour pouvoir affecter l'image pour ne pas avoir l'erreur clé étrangère inconnu
+        // Attraper le dernier id de la table products créé pour ensuite faire la query
+        // de l'image et insérer l'image avec la clé étrangère id du produit
+        $query = 'SELECT LAST_INSERT_ID() FROM products';
+        $statement = $connexion->prepare($query);
+        $statement->execute();
+        $result = $statement->fetch(PDO::FETCH_ASSOC);
+        $last_id = $result['LAST_INSERT_ID()']; // stocker le dernier id de la table products dans une variable
+        // echo "ID du produit $last_id";
+        // die();
+    
+
+        // attraper le dernier id de la table products pour ensuite faire la query
+        // insérer dans la table image
+
+        // ensuite aller choper l'id du produit que je viens d'ajouter pour pouvoir affecter l'image 
+        // pour ne pas avoir l'erreur clé étrangère inconnu
 
         // Je peux faire plusieur queries pour ajouter les images et les catégories dans les autres tables correspondantes
         // Refaire la requette image qui ne se fait pas bien, il faut un JOIN ou autre pour dire que si cat
         // catégorie choisi = promo alors id_cat = 1, si catégorie choisi = nouveauté alors id_cat = 2, si catégorie choisi = kit/pack alors id_cat = 3
-        $query = 'INSERT INTO image (image_path) VALUES (:image_path)';
+        $query = 'INSERT INTO image (id_product, image_path) VALUES (:id_product, :image_path)';
         $statement = $connexion->prepare($query);
+        $statement->bindParam(':id_product', $last_id);
         $statement->bindParam(':image_path', $this->image_path);
         $statement->execute();
+
+        // Faire la query de la catégorie
     }
 }
 ?>
