e00845a8d41b38731ea2546e3f16bf66fe3d84f3
Julien Lach <leadedchamps@gmail.com>
2024-03-18 11:01:58 +0100

    fix ajouter au panier sur la page product.php

--- view/product.php
+++ view/product.php
@@ -35,7 +35,10 @@
                         </ul>
                     </div>
                     <div>
-                        <button class="add-to-cart-btn">Ajouter au panier</button>
+                        <form action="../controller/add-to-shopping-cart.php" method="POST">
+                            <input type="hidden" name="productId" value="<?= $product['id_product'];?>">
+                            <button type="submit" class="add-to-cart-btn">Ajouter au panier</button>
+                        </form>
                     </div>
                 </div>
 
