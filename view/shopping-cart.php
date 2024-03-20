963e14a59f47738bef3be219105ef52cd9ac7387
Julien Lach <leadedchamps@gmail.com>
2024-03-20 12:05:54 +0100

    fix bouton ajouter au panier si pas de produit

--- view/shopping-cart.php
+++ view/shopping-cart.php
@@ -66,9 +66,16 @@
                     <div class="shopping-cart-menu">
                         <div class="black-separator"></div>
                         <h5>Sous total : <span><?= empty($products) ? 0 : end($products)['total'] ?> €</span></h5>                        <div>
-                            <a href="../controller/confirm-shopping-cart.php">
+                            <?php if(!isset($products)) {
+                            echo '<a href="index.php"><button class="shopping-cart-validate-btn">Retour à l\'accueil</button></a>';
+                            } else {
+                            echo '<a href="../controller/confirm-shopping-cart.php"><button class="shopping-cart-validate-btn">Valider mon panier</button></a>';
+                            }
+                            ?>
+                            
+                            <!-- <a href="../controller/confirm-shopping-cart.php">
                                 <button class="shopping-cart-validate-btn">Valider mon panier</button>
-                            </a>
+                            </a> -->
                         </div>
                     </div>
                 </div>
