912eadb563c693ad07dcb744d20ee2d53c42a6ba
Julien Lach <leadedchamps@gmail.com>
2024-03-27 14:38:29 +0100

    fix mentions légales

--- view/mentions-legales.php
+++ view/mentions-legales.php
@@ -12,33 +12,22 @@
             <h3>Mentions légales</h3>
             <h4>Éditeur</h4>
             <p>
-            Vous êtes actuellement connecté au site web de l’entreprise HomeIOT
+            Vous êtes actuellement connecté au site web de l’entreprise HomeIOT.
             Ce site est la propriété de HomeIOT situé au 314 rue Louis Lumière 76620 Le Havre.
             </p>
             <h4>Droit d’auteur – Copyright : </h4>
             <p>
-            L’ensemble de ce site relève de la législation française et internationale sur le droit d’auteur et la propriété intellectuelle. La reproduction de tout ou partie de ce site, de sa charte graphique, des éléments et informations qu’il contient sur quelque support 
-            que ce soit est formellement interdite sauf autorisation expresse de HomeIOT.
-            Crédits photos : Raspberry Pi ; Arduino.
+            Ce site est protégé par la législation française et internationale sur le droit d'auteur et la propriété intellectuelle. Toute reproduction, totale ou partielle, de son contenu, de sa charte graphique, ainsi que des éléments et informations qu'il contient, sur quelque support que ce soit, est strictement interdite sans l'autorisation expresse de HomeIOT. Crédits photos : Raspberry Pi ; Arduino.
             </p>
             <h4>Protection des données personnelles:</h4>
             <p>
-            ERRA éditeur du site, en qualité de responsable du traitement, 
-            met en œuvre un traitement de données à caractère personnel ayant pour 
-            principales finalités de constituer et exploiter un fichier de courriels à 
-            des fins d’envoi de cette lettre mensuelles.
+            En tant que responsable du traitement des données, HomeIOT, éditeur du site, met en place un traitement de données à caractère personnel dans le but principal de constituer et exploiter une liste d'adresses électroniques pour l'envoi de lettres d'information mensuelles.
             </p>
             <p>
-            Ces informations sont destinées à l’éditeur du site, ainsi que, pour les finalités 
-            susvisées, aux prestataires contractuels. Elles seront conservées pendant une durée de 
-            3 ans, durée qui est nécessaire à la réalisation de des finalités susvisées. 
+            Ces informations sont destinées à l'éditeur du site ainsi qu'à ses prestataires contractuels, dans le cadre des finalités susmentionnées. Elles seront conservées pendant une durée de 3 ans, nécessaire à la réalisation de ces objectifs. 
             </p>
             <p>
-            Ces informations sont destinées à l’éditeur du site, ainsi que, pour les finalités susvisées, aux éventuels prestataires contractuels.
-            Conformément aux dispositions applicables en matière de protection des données 
-            à caractère personnel, vous bénéficiez d’un droit d’interrogation, d’accès, de rectification, d’effacement et de portabilité de vos données, ainsi que du droit d’obtenir la limitation de leur traitement et d’un droit d’opposition (au traitement de vos données, ainsi qu’à la prospection notamment commerciale).
-            Vous disposez en tout état de cause de la possibilité d’introduire une réclamation 
-            auprès d’une autorité nationale en charge de la protection des données à caractère personnel (en France, il s’agit de la Commission Nationale de l’Informatique et des Libertés ou « CNIL ») si vous estimez que le traitement de vos données n’est pas effectué conformément aux dispositions légales et réglementaires applicables
+            Conformément à la réglementation sur la protection des données personnelles, vous disposez du droit d'interroger, d'accéder, de rectifier, d'effacer et de transférer vos données, ainsi que du droit de limiter leur traitement et de vous opposer à celui-ci (notamment pour des activités de prospection commerciale). Vous avez également la possibilité de déposer une réclamation auprès de l'autorité nationale compétente en matière de protection des données personnelles (en France, la Commission Nationale de l'Informatique et des Libertés ou CNIL), si vous estimez que le traitement de vos données ne respecte pas la législation en vigueur.
             </p>
           </div>
         </div>
